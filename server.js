233e25a0292c0101ad26427705c6339b03f7da14
tiffani <tiffanipwhite12@gmail.com>
2026-02-13 21:49:04 -0500

    Appointment Settings 4

--- server.js
+++ server.js
@@ -39,8 +39,16 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Accept", "Authorization", "X-Requested-With"],
 }));
 
-app.options("*", (req, res) => res.sendStatus(204));
-
+app.options("*", cors({
+  origin(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) return callback(null, true);
+    return callback(new Error("Not allowed by CORS: " + origin));
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Accept", "Authorization", "X-Requested-With"],
+}));
 
 const mongoose = require('mongoose');
 const fs = require('fs');
